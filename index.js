bc71292c8b4567e13b9d62563570b00138cd4210
John-Giovenco <JohnGiovenco93@gmail.com>
2023-03-01 21:39:20 -0800

    commit step 4 start

--- index.js
+++ index.js
@@ -2,98 +2,74 @@ const inventory = newInventory()
 move(inventory).to(0, 0)
 
 const character = newImage('assets/green-character/static.gif')
-    let direction = null;
-    let x = 100;
-    let y = 250;
+move(character).withArrowKeys
+let x = 100;
+let y = 250;
 
-setInterval(function() {
-    if(direction === 'west') {
-        x = x - 1
+function moveCharacter() {
+    function move(element) {
+        element.style.position = 'fixed'
+    
+        function moveToCoordinates(left, bottom) {
+            element.style.left = left + 'px'
+            element.style.bottom = bottom + 'px'
+        }
+        function moveWithArrowKeys(left, bottom){
+            let direction = null;
+            let x = left;
+            let y = bottom;
+            
+            element.style.left = x + 'px'
+            element.style.bottom = y + 'px'
+                
+            function moveCharacter(){ 
+                if(direction === 'west'){
+                    x-=1
+                }
+                if(direction === 'north'){
+                    y+=1
+                }
+                if(direction === 'east'){
+                    x+=1
+                }
+                if(direction === 'south'){
+                    y-=1
+                }
+                element.style.left = x + 'px'
+                element.style.bottom = y + 'px'
+                }
+                
+                setInterval(moveCharacter, 1)
+                
+                document.addEventListener('keydown', function(e){
+                    if(e.repeat) return;
+                
+                    if(e.key === 'ArrowLeft'){
+                        direction = 'west'
+                    }
+                    if(e.key === 'ArrowUp'){
+                        direction = 'north'
+                    }
+                    if(e.key === 'ArrowRight'){
+                        direction = 'east'
+                    }
+                    if(e.key === 'ArrowDown'){
+                        direction = 'south'
+                    }
+                })
+                
+                document.addEventListener('keyup', function(e){
+                    direction = null
+                })
+        }      
     }
-    if(direction === 'north'){
-        y = y + 1
-    }    
-    if(direction === 'east'){
-        x = x + 1
-    }    
-    if(direction === 'south'){
-        y = y - 1
+    return {
+        to: moveToCoordinates,
+        withArrowKeys: moveWith
     }
-    character.style.left = x + 'px'
-    character.style.bottom = y + 'px'
-}, 1)       
-
-document.addEventListener('keydown', function(e) {   
-    if(e.repeat) return;
-
-    if(e.key === 'ArrowLeft'){
-        direction = 'west'
-    }
-    if(e.key === 'ArrowUp'){
-        direction = 'north'
-    }
-    if(e.key === 'ArrowRight'){
-        direction = 'east'
-    }
-    if(e.key === 'ArrowDown'){
-        direction = 'south'
-    }
-})
-
-document.addEventListener('keyup', function(e){
-    direction = null
-})
+}                              
 
 move(character).to(100, 250)
-
-function moveWithArrowKeys(left, bottom){
-    let direction = null;
-    let x = left;
-    let y = bottom;
-
-    element.style.left = x + 'px'
-    element.style.bottom = y + 'px'
-    
-    function moveCharacter(){ 
-        if(direction === 'west'){
-            x-=1
-        }
-        if(direction === 'north'){
-            y+=1
-        }
-        if(direction === 'east'){
-            x+=1
-        }
-        if(direction === 'south'){
-            y-=1
-        }
-        element.style.left = x + 'px'
-        element.style.bottom = y + 'px'
-    }
-    
-    setInterval(moveCharacter, 1)
-    
-    document.addEventListener('keydown', function(e){
-        if(e.repeat) return;
-    
-        if(e.key === 'ArrowLeft'){
-            direction = 'west'
-        }
-        if(e.key === 'ArrowUp'){
-            direction = 'north'
-        }
-        if(e.key === 'ArrowRight'){
-            direction = 'east'
-        }
-        if(e.key === 'ArrowDown'){
-            direction = 'south'
-        }
-    })
-    
-    document.addEventListener('keyup', function(e){
-        direction = null
-    })
-}
 
 move(newImage('assets/tree.png')).to(200, 450)
 move(newImage('assets/pillar.png')).to(350, 250)
